13de45311cd46a3439751efd3502bda840d2aa82
Jens de Waard <jensdewaard@gmail.com>
2015-10-13 15:30:34 +0200

    Create Route class

---
+++ src/AntManager.java
@@ -0,0 +1,10 @@
+public class AntManager {
+    private Maze maze;
+
+    private int MAX_ANTS;
+    private int MAX_ITERATIONS;
+
+    public AntManager(Maze maze) {
+        this.maze = maze;
+    }
+}
