00a8c290b6ffc4122623a50db30d5f5e65d27376
Jens de Waard <jensdewaard@gmail.com>
2015-10-13 14:28:12 +0200

    Create Point class

---
+++ src/Point.java
@@ -0,0 +1,15 @@
+public final class Point {
+    private final int x, y;
+    public Point(final int x, final int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+}
